3033d36fbcce2ec394591b885a10b2999beb458a
Matheus Sá Cardoso <78807235+matheussacardoso@users.noreply.github.com>
2022-06-16 00:59:20 -0300

    Formato de projeto java correto

---
+++ CampoMinadoLP1/src/campominadolp1/JButtonGrade.java
@@ -0,0 +1,34 @@
+package campominadolp1;
+
+import javax.swing.JButton;
+
+public class JButtonGrade extends JButton {
+
+    int linha;
+    int coluna;
+    Campo c;
+    String text;
+
+    public JButtonGrade(Campo c, JFrameCampo cpg) {
+        //this.campoGrafico =cpg;
+        this.text = "";
+        this.setText(text);
+        this.c = c;
+        this.addActionListener((java.awt.event.ActionEvent evt) -> {
+            Pressionado(evt);
+        });
+    }
+
+    private void Pressionado(java.awt.event.ActionEvent evt) {
+        int ret = c.clicar(linha, coluna);
+        this.text = Integer.toString(ret);
+        this.setText(text);
+        this.setEnabled(false);
+    }
+
+    public void setPos(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+}
